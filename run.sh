2d586de9be9906cbc5519c094359c3311ab44353
Vishnu Menon <me@vishnumenon.com>
2023-05-08 13:22:57 -0700

    Initial Commit

---
+++ run.sh
@@ -0,0 +1,1 @@
+./result/bin/llama -m ../../weights/koala-cpp/koala-7B-4bit-128g.GGML.bin -c 2048 --temp 0.7 --repeat_penalty 1.1 -n -1 -i -ins
---
+++ server/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { execaCommand, execaCommandSync } from "execa";
+
+const app = express();
+const port = 9801;
+
+app.use(bodyParser.json());
+
+execaCommandSync("chmod +x ./run.sh");
+const modelProcess = execaCommand("./run.sh");
+let modelDataListener = (data) => {
+  console.log(data.toString());
+};
+modelProcess.stdout.on("data", modelDataListener);
+modelProcess.pipeStderr(process.stderr);
+
+app.post("/chat", async (req, res) => {
+  const { message } = req.body;
+  modelProcess.stdout.removeListener("data", modelDataListener);
+  let responseMessage = "";
+  modelDataListener = (data) => {
+    responseMessage += data.toString();
+    if (responseMessage.endsWith("### Instruction:\n\n\n> ")) {
+      res.send({
+        message: responseMessage.substring(0, responseMessage.length - 21),
+      });
+    } else if (responseMessage.endsWith("> ")) {
+      res.send({
+        message: responseMessage.substring(0, responseMessage.length - 2),
+      });
+    }
+  };
+
+  modelProcess.stdout.on("data", modelDataListener);
+  modelProcess.stdin.write(message + "\n");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
